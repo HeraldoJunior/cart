3ce53eb56cf922d60a7c56fffbe04c3b1c29cabf
Heraldo Junior <heraldo_junior@live.com>
2019-04-24 21:24:01 -0300

    alterado ip do serviço

--- src/main/java/cart/client/ProdutoClient.java
+++ src/main/java/cart/client/ProdutoClient.java
@@ -12,7 +12,7 @@ public class ProdutoClient {
     private RestOperations restOperations;
     private String productServiceUrl;
 
-    public ProdutoClient(RestOperations restOperations, @Value("http://34.74.157.7:8888") String productServiceUrl) {
+    public ProdutoClient(RestOperations restOperations, @Value("http://35.247.196.46:8888") String productServiceUrl) {
         this.restOperations = restOperations;
         this.productServiceUrl = productServiceUrl;
     }
